4afcf8e47392a55ca674c3f8b9f33fa5b705593b
Makara Wang <makara15@gmail.com>
2016-03-16 19:35:20 +0800

    Added the connection.

---
+++ test/init.js
@@ -0,0 +1,20 @@
+'use strict';
+
+var Promise = require('bluebird');
+var DataSource = require('loopback-datasource-juggler').DataSource;
+
+var config = {};
+
+exports.getDataSource = function (customConfig, callback) {
+  var promise = new Promise(function (resolve, reject) {
+    var db = new DataSource(require('../'), customConfig || config);
+    db.log = function (a) {
+      console.log(a);
+    };
+    db.on('connected', function () {
+      resolve(db);
+    });
+    db.on('error', reject);
+  });
+  return promise.asCallback(callback);
+};
