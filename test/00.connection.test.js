7bc832317e75a3cb678280ff425f6d946c56e576
Makara Wang <makara15@gmail.com>
2016-03-29 21:08:29 +0800

    Improving save and find hooks.

--- test/00.connection.test.js
+++ test/00.connection.test.js
@@ -10,7 +10,7 @@ describe('Redis connector', function () {
   var db;
   var connector;
 
-  it('can connect.', function (done) {
+  it('can connect', function (done) {
     init.getDataSource(null, function (err, res) {
       if (err) return done(err);
       res.should.be.Object();
@@ -22,7 +22,7 @@ describe('Redis connector', function () {
     });
   });
 
-  it('can connect.', function (done) {
+  it('can connect', function (done) {
     connector.connect(function (err, res) {
       if (err) return done(err);
       res.should.be.instanceof(Redis);
@@ -30,11 +30,11 @@ describe('Redis connector', function () {
     });
   });
 
-  it('can disconnect.', function (done) {
+  it('can disconnect', function (done) {
     db.disconnect(done);
   });
 
-  it('can disconnect.', function (done) {
+  it('can disconnect', function (done) {
     connector.disconnect(function (err, res) {
       if (err) return done(err);
       res.should.equal(true);
@@ -42,19 +42,75 @@ describe('Redis connector', function () {
     });
   });
 
-  it('can connect twice the same time.', function (done) {
+  it('can connect twice the same time', function (done) {
     connector.connect();
     connector.connect(done);
   });
 
-  it('can disconnect twice the same time.', function (done) {
+  it('can disconnect twice the same time', function (done) {
     connector.disconnect();
     connector.disconnect(done);
   });
 
-  it('can connect and disconnect.', function (done) {
+  it('can connect and disconnect', function (done) {
     connector.connect();
     connector.disconnect(done);
+  });
+
+  it('can connect with a host', function (done) {
+    init.getDataSource({
+      host: 'localhost'
+    }).then(function (res) {
+      res.should.be.Object();
+      res.should.have.property('connected', true);
+      res.should.have.property('connector').which.is.Object();
+      res.disconnect(done);
+    }).catch(done);
+  });
+
+  it('can connect with a port', function (done) {
+    init.getDataSource({
+      port: '6379'
+    }).then(function (res) {
+      res.should.be.Object();
+      res.should.have.property('connected', true);
+      res.should.have.property('connector').which.is.Object();
+      res.disconnect(done);
+    }).catch(done);
+  });
+
+  it('can connect with a host and a port', function (done) {
+    init.getDataSource({
+      host: 'localhost',
+      port: '6379'
+    }).then(function (res) {
+      res.should.be.Object();
+      res.should.have.property('connected', true);
+      res.should.have.property('connector').which.is.Object();
+      res.disconnect(done);
+    }).catch(done);
+  });
+
+  it('cannot connect with a wrong port', function (done) {
+    init.getDataSource({
+      port: '1234'
+    }).then(function (res) {
+      done(new Error('expected an error'));
+    }).catch(function (err) {
+      err.should.be.instanceof(Error);
+      done();
+    });
+  });
+
+  it('can connect with a URL', function (done) {
+    init.getDataSource({
+      url: 'redis://localhost:6379'
+    }).then(function (res) {
+      res.should.be.Object();
+      res.should.have.property('connected', true);
+      res.should.have.property('connector').which.is.Object();
+      res.disconnect(done);
+    }).catch(done);
   });
 
 });
